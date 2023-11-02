a991b0a92fd355180fe158edaa1be11c6f00732c
JorgeGonzales15 <u202118468@upc.edu.pe>
2023-11-02 04:30:49 -0500

    fix asdasd

--- src/main/java/com/greatminds/ayni/AyniBackendApplication.java
+++ src/main/java/com/greatminds/ayni/AyniBackendApplication.java
@@ -2,12 +2,25 @@ package com.greatminds.ayni;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AyniBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AyniBackendApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1").allowedOrigins("https://monorail.proxy.rlwy.net:59822");
+            }
+        };
     }
 
 }
